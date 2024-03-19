9dc7a0ad7d59dcb70162ec42cb51048976b4de88
Steven Cocks <ext.steven.cocks@king.com>
2024-03-19 15:45:51 +0100

    refactor methods

--- src/main/java/org/king/plataforma/Library.java
+++ src/main/java/org/king/plataforma/Library.java
@@ -1,52 +1,27 @@
 package org.king.plataforma;
 
-import java.util.ArrayList;
-
-public class Library {
-    public String[] count(Integer number) {
-        String[] lines = new String[number];
-        for (int i = 0; i < number; i++) {
-            lines[i] = "i = " + i;
-        }
-        return lines;
-    }
-    public ArrayList<String> greetAll(String name) {
-        ArrayList<String> greetings = new ArrayList<>();
-        org.king.libDep1.Library libDep1 = new org.king.libDep1.Library();
-        org.king.libDep2.Library libDep2 = new org.king.libDep2.Library();
-        org.king.libDep3.Library libDep3 = new org.king.libDep3.Library();
-        org.king.libDep4.Library libDep4 = new org.king.libDep4.Library();
-        org.king.libDep5.Library libDep5 = new org.king.libDep5.Library();
-        greetings.add("Hello from plataforma, " + name + "!");
-        greetings.add(libDep1.greet(name));
-        greetings.add(libDep2.greet(name));
-        greetings.add(libDep3.greet(name));
-        greetings.add(libDep4.greet(name));
-        greetings.add(libDep5.greet(name));
-        return greetings;
-    }
+public class Library {    
     public String greet(String name) {
-        return "Hello from plataforma, " + name + "!";
+        return "greet from plataforma, " + name + "!";
     }
-
     public String greet1(String name) {
-        org.king.libDep1.Library libDep1 = new org.king.libDep1.Library();
-        return libDep1.greet(name);
+        org.king.libdep1.Library libDep1 = new org.king.libdep1.Library();
+        return "greet1 from plataforma " + libDep1.greet(name);
     }
     public String greet2(String name) {
-        org.king.libDep2.Library libDep2 = new org.king.libDep2.Library();
-        return libDep2.greet(name);
+        org.king.libdep2.Library libDep2 = new org.king.libdep2.Library();
+        return "greet2 from plataforma " + libDep2.greet(name);
     }
     public String greet3(String name) {
-        org.king.libDep3.Library libDep3 = new org.king.libDep3.Library();
-        return libDep3.greet(name);
+        org.king.libdep3.Library libDep3 = new org.king.libdep3.Library();
+        return "greet3 from plataforma " + libDep3.greet(name);
     }
     public String greet4(String name) {
-        org.king.libDep4.Library libDep4 = new org.king.libDep4.Library();
-        return libDep4.greet(name);
+        org.king.libdep4.Library libDep4 = new org.king.libdep4.Library();
+        return "greet4 from plataforma " + libDep4.greet(name);
     }
     public String greet5(String name) {
-        org.king.libDep5.Library libDep5 = new org.king.libDep5.Library();
-        return libDep5.greet(name);
+        org.king.libdep5.Library libDep5 = new org.king.libdep5.Library();
+        return "greet5 from plataforma " + libDep5.greet(name);
     }
 }
--- src/test/java/org/king/plataforma/MainTest.java
+++ src/test/java/org/king/plataforma/MainTest.java
@@ -2,66 +2,43 @@ package org.king.plataforma;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MainTest {
-    @Test
-    void testCount() {
-        Library library = new Library();
-        String[] result = library.count(10);
-        assertEquals(10, result.length);
-        for (int i = 0; i < 10; i++) {
-            assertEquals("i = " + i, result[i]);
-        }
-    }
-    @Test
-    void testGreetAll() {
-        Library library = new Library();
-        ArrayList<String> result = library.greetAll("plataforma");
-        assertEquals(6,result.size());
-        assertEquals("Hello from plataforma, plataforma!", result.get(0));
-        assertEquals("Hello, plataforma!", result.get(1));
-        assertEquals("Greetings, plataforma!", result.get(2));
-        assertEquals("Salutations, plataforma!", result.get(3));
-        assertEquals("Hej, plataforma!", result.get(4));
-        assertEquals("Kon'nichiwa, plataforma!", result.get(5));
-    }
+class MainTest {    
     @Test
     void testGreet() {
         Library library = new Library();
         String result = library.greet("plataforma");
-        assertEquals("Hello from plataforma, plataforma!", result);
+        assertEquals("greet from plataforma, plataforma!", result);
     }
     @Test
     void testGreet1() {
         Library library = new Library();
         String result = library.greet1("plataforma");
-        assertEquals("Hello, plataforma!", result);
+        assertEquals("greet1 from plataforma greet from libdep1, plataforma!", result);
     }
     @Test
     void testGreet2() {
         Library library = new Library();
         String result = library.greet2("plataforma");
-        assertEquals("Greetings, plataforma!", result);
+        assertEquals("greet2 from plataforma greet from libdep2, plataforma!", result);
     }
     @Test
     void testGreet3() {
         Library library = new Library();
         String result = library.greet3("plataforma");
-        assertEquals("Salutations, plataforma!", result);
+        assertEquals("greet3 from plataforma greet from libdep3, plataforma!", result);
     }
     @Test
     void testGreet4() {
         Library library = new Library();
         String result = library.greet4("plataforma");
-        assertEquals("Hej, plataforma!", result);
+        assertEquals("greet4 from plataforma greet from libdep4, plataforma!", result);
     }
     @Test
     void testGreet5() {
         Library library = new Library();
         String result = library.greet5("plataforma");
-        assertEquals("Kon'nichiwa, plataforma!", result);
+        assertEquals("greet5 from plataforma greet from libdep5, plataforma!", result);
     }
 }
